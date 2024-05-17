466f8a5dcef19a49333da4f59b286442317fe772
Tito Oktavian <tito@majoo.id>
2024-05-18 00:35:24 +0700

    change select icon

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
-import { LucideIcon } from "lucide-react";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
