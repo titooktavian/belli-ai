6b6c7a059a2511e7969a9cbb14457963568d9f22
Tito Oktavian <tito@majoo.id>
2024-05-18 00:05:22 +0700

    modify input component to have left icon and right icon

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -6,17 +6,32 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, leftIcon, rightIcon, ...props }, ref) => {
+    console.log(rightIcon)
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="w-full relative">
+        {leftIcon && (
+          <div className="absolute left-1.5 top-1/2 transform -translate-y-1/2">
+            {leftIcon}
+          </div>
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={type}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            leftIcon ? "pl-8" : "",
+            rightIcon ? "pr-8" : "",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {rightIcon && (
+          <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+            {rightIcon}
+          </div>
+        )}
+      </div>
     )
   }
 )
