7c6e1c68ce11e491e35528a32c20d400ecfae643
Tito Oktavian <tito@majoo.id>
2024-05-17 23:05:58 +0700

    booking type

--- components/order/dialog.tsx
+++ components/order/dialog.tsx
@@ -7,14 +7,30 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { ClockIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { ClockIcon, MagnifyingGlassIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
 import { Button } from "../ui/button";
-import { Cross2Icon, SizeIcon } from "@radix-ui/react-icons";
+import { Cross2Icon, InfoCircledIcon, ReloadIcon, SizeIcon } from "@radix-ui/react-icons";
 import { Card, CardContent, CardFooter, CardTitle } from "../ui/card";
 import { Separator } from "../ui/separator";
 import Link from "next/link";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { Package, PlaneIcon, ScrollText, UserRound } from "lucide-react";
+import { Checkbox } from "../ui/checkbox";
   
 const OrderDialog = () => {
   return (
@@ -102,19 +118,147 @@ const OrderDialog = () => {
                     <Card
                       className="overflow-hidden rounded-lg border-[1px] bg-transparent border-zinc-700 text-zinc-400"
                     >
-                      <CardContent className="p-3 flex flex-col gap-4">
-                        <div className="flex justify-between">
-                          <h2 className="text-xl font-semibold text-white">Balance</h2>
-                          <h2 className="text-xl text-white">$ 0.00</h2>
+                      <CardContent className="p-3 flex flex-col gap-4 py-4">
+                        <div className="flex flex-col gap-2">
+                          <div className="flex gap-1 items-center text-white">
+                            <div className="text-sm">Booking Type</div>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <InfoCircledIcon />
+                                </TooltipTrigger>
+                                <TooltipContent side="right">
+                                  <p>Lorem</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </div>
+                          <Select>
+                            <SelectTrigger className="w-[180px] border-zinc-700 w-full">
+                              <SelectValue placeholder="" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectItem value="TYPE-1">TYPE-1</SelectItem>
+                                <SelectItem value="TYPE-2">TYPE-2</SelectItem>
+                                <SelectItem value="TYPE-3">TYPE-3</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
                         </div>
 
-                        <Separator className="pl-2 pr-2 bg-zinc-700" />
+                        <div className="flex flex-col gap-2">
+                          <div className="flex gap-1 items-center text-white">
+                            <div className="text-sm">Partner Prefix *</div>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <InfoCircledIcon />
+                                </TooltipTrigger>
+                                <TooltipContent side="right">
+                                  <p>Lorem</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </div>
+                          <Select>
+                            <SelectTrigger className="w-[180px] border-zinc-700 w-full">
+                              <SelectValue placeholder="" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectItem value="A1">A1</SelectItem>
+                                <SelectItem value="B2">B2</SelectItem>
+                                <SelectItem value="C3">C3</SelectItem>
+                                <SelectItem value="D4">D4</SelectItem>
+                                <SelectItem value="E5">E5</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        </div>
 
-                        <h2 className="text-base font-semibold">Elroy Carreen</h2>
+                        <div className="flex flex-col gap-2">
+                          <div className="flex gap-1 items-center text-white">
+                            <div className="text-sm">AWB#</div>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <InfoCircledIcon />
+                                </TooltipTrigger>
+                                <TooltipContent side="right">
+                                  <p>Lorem</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </div>
+                          <Select>
+                            <SelectTrigger className="w-[180px] border-zinc-700 w-full">
+                              <SelectValue placeholder="" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectItem value="AWB001">AWB001</SelectItem>
+                                <SelectItem value="AWB002">AWB002</SelectItem>
+                                <SelectItem value="AWB003">AWB003</SelectItem>
+                                <SelectItem value="AWB004">AWB004</SelectItem>
+                                <SelectItem value="AWB005">AWB005</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        </div>
 
-                        <div className="flex justify-between">
-                          <h2 className="text-base">Individual Balance</h2>
-                          <h2 className="text-base">$ 0.00</h2>
+                        <div className="flex flex-col gap-2">
+                          <div className="flex gap-1 items-center text-white">
+                            <div className="text-sm">Partner Code *</div>
+                            <TooltipProvider>
+                              <Tooltip>
+                                <TooltipTrigger asChild>
+                                  <InfoCircledIcon />
+                                </TooltipTrigger>
+                                <TooltipContent side="right">
+                                  <p>Lorem</p>
+                                </TooltipContent>
+                              </Tooltip>
+                            </TooltipProvider>
+                          </div>
+                          <Select>
+                            <SelectTrigger className="w-[180px] border-zinc-700 w-full">
+                              <SelectValue placeholder="" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectGroup>
+                                <SelectItem value="A1">A1</SelectItem>
+                                <SelectItem value="B2">B2</SelectItem>
+                                <SelectItem value="C3">C3</SelectItem>
+                                <SelectItem value="D4">D4</SelectItem>
+                                <SelectItem value="E5">E5</SelectItem>
+                              </SelectGroup>
+                            </SelectContent>
+                          </Select>
+                        </div>
+
+                        <div className="flex gap-3">
+                          <div className="flex items-center space-x-2">
+                            <Checkbox id="isPhysical" className="border-zinc-500" />
+                            <label
+                              htmlFor="isPhysical"
+                              className="text-sm text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            >
+                              Is Physical
+                            </label>
+                          </div>
+                          <Button className="bg-button-primary" size="icon">
+                            <MagnifyingGlassIcon
+                              className="h-5 w-5 text-white"
+                              aria-hidden="true"
+                            />
+                          </Button>
+                          <Button className="bg-button-primary" size="icon">
+                            <ReloadIcon
+                              className="h-5 w-5 text-white"
+                              aria-hidden="true"
+                            />
+                          </Button>
                         </div>
                       </CardContent>
                     </Card>
